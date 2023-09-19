fb274d777e5e7b17a48bcedfe26ff4d036816c17
Bharat <65550867+bharatk098@users.noreply.github.com>
2023-09-19 11:29:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,35 @@
-// Get elements by their IDs
-const inputText = document.getElementById("inputText");
-const generateButton = document.getElementById("generateButton");
-const qrcode = document.getElementById("qrcode");
-const downloadButton = document.getElementById("downloadButton");
+const generateButton = document.getElementById('generate-button');
+const textInput = document.getElementById('text-input');
+const qrCodeContainer = document.getElementById('qr-code-container');
+const downloadLink = document.getElementById('download-link');
 
+generateButton.addEventListener('click', async () => {
+    const text = textInput.value;
+    if (text.trim() !== '') {
+        const apiEndpoint = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(
+            text
+        )}`;
 
-generateButton.addEventListener("click", function () {
-    const inputValue = inputText.value;
+        try {
+            const response = await fetch(apiEndpoint);
+            if (response.ok) {
+                const data = await response.blob();
 
-    if (inputValue.trim() !== "") {
-        
-        qrcode.innerHTML = "";
+                // Display QR code image
+                const qrCodeImage = document.createElement('img');
+                qrCodeImage.src = URL.createObjectURL(data);
+                qrCodeContainer.innerHTML = '';
+                qrCodeContainer.appendChild(qrCodeImage);
 
-        
-        const qr = new QRCode(qrcode, {
-            text: inputValue,
-            width: 200,
-            height: 200,
-        });
-
-        
-        downloadButton.style.display = "block";
+                // Show download link
+                downloadLink.style.display = 'inline-block';
+                downloadLink.href = qrCodeImage.src;
+                downloadLink.download = 'qrcode.png';
+            } else {
+                throw new Error('Failed to generate QR code');
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
-});
-
-
-downloadButton.addEventListener("click", function () {
-
-    const timestamp = Date.now();
-
-   
-    const a = document.createElement("a");
-
-    
-    a.href = qrcode.querySelector("img").src;
-
-    
-    a.download = `qr-code-${timestamp}.png`;
-
-    
-    a.click();
 });
